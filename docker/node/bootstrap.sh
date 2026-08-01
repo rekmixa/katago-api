2b7970e40091a7454de3af19251959e2cfcbdacd
rekmixa <rekmixa@gmail.com>
2026-08-02 00:36:10 +0500

    change bootstrap.sh

--- docker/node/bootstrap.sh
+++ docker/node/bootstrap.sh
@@ -5,7 +5,7 @@ MODEL_PATH="${KATAGO_MODEL:-/home/node/app/katago/models/default.bin.gz}"
 MODEL_URL="${KATAGO_MODEL_URL:-}"
 
 if [ -n "$MODEL_URL" ] && [ ! -f "$MODEL_PATH" ]; then
-  echo "Downloading KataGo model to $MODEL_PATH"
+  echo "Downloading KataGo model $MODEL_URL to $MODEL_PATH"
   mkdir -p "$(dirname "$MODEL_PATH")"
   curl -fsSL "$MODEL_URL" -o "$MODEL_PATH"
 fi
